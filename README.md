6a4d983fadea8c654c8d771f76b5ec3ee1066755
Gilles.taddei <gilles.taddei@thalesdigital.io>
2021-01-08 10:40:09 +0100

    adding comments, and README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # soundboard
+
+## Présentation
+Ce projet a pour but d'ajouter des raccourcis clavier facilement.
+
+## Installation
+Pour réécrire le code fourni, il est nécessaire d'installer l'IDE Arduino [trouvable ici](https://www.arduino.cc/).
+
+Une fois Arduino installé et lancé, ouvrir le fichier [soundBoard.ino](./soundBoard/soundBoard.ino).
+
+Il est maintenant possible de modifier et charger le code.
+
+Pensez à configurer le microcontrôleur dans le menu outils:
+![](documentation/defineTypeAndPort.png)
+
+Définir le Type de carte sur Arduino Leonardo, et le Port sur le port où est branché la carte.
+
+Une fois les modifications faites, téléverser le code:
+![](documentation/upload.png)
+
+And voilà ! :)
--- soundBoard/soundBoard.ino
+++ soundBoard/soundBoard.ino
@@ -4,115 +4,123 @@
 int val = 0;
 int prevAnalogVal = 0;
 char prevVal = ' ';
-char lastVal = ' ';
+char currentVal = ' ';
 
 double lastTimeCall = millis();
 
-// value to determine the size of the readings array.
+// La taille du tampon pour le lissage des valeurs lues
 const int numReadings = 20;
 
-int readings[numReadings];      // the readings from the analog input
-int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
-bool skipped = false;
+int readings[numReadings];      // le tampon pour le lissage des valeurs lues
+int readIndex = 0;              // L'index de la lecture courante
+int total = 0;                  // La somme des valeurs lues
+int average = 0;                // La moyenne des valeurs lues
+bool skipped = false;           // Un booléen pour
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  // initialize all the readings to 0:
+  // On met toutes les valeurs à 0
   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     readings[thisReading] = 0;
   }
 }
 
 void loop() {
-  // We tempo a bit to avoid wrong average
-  if (prevAnalogVal < 25){
+  analogVal = analogRead(btns);
+
+  //on remet la valeur moyennée à 0
+  val = 0
+  // Si la valeur est inférieure à un seuil arbitraire, on met un délais et on saute le traitement
+  if (analogVal < 25){
     delay(200);
   }
-  int valTmp = 0;
+
   for (int i = 0; i < numReadings; ++i){
-    // subtract the last reading:
-    total = total - readings[readIndex];
-    // read from the sensor:
-    valTmp = analogRead(btns);
-    readings[readIndex] = valTmp;
-    // add the reading to the total:
+    // On soustrait la valeur lue dans la "case" du tampon qu'on va remplir
+    total = total - readings[readIndex]
+    readings[readIndex] = analogVal;
+    // On ajoute la valeur lue au total
     total = total + readings[readIndex];
-    // advance to the next position in the array:
-    readIndex = (readIndex + 1) % numReadings;
-    // if we're at the end of the array...
+    // On passe à la case suivante
+    readIndex++;
+    // On retourne au début du tableau si on en a atteint la fin
     if (readIndex >= numReadings) {
-      // ...wrap around to the beginning:
       readIndex = 0;
     }
   }
-  // calculate the average:
+  // On calcule la moyenne:
   average = total / numReadings;
-  // send it to the computer as ASCII digits
-  // Serial.println(average);
-
   val = average;
-  
+
+  //Pour tout l'enchaînement à suivre, on affecte une lettre à une variable
   if (val >= 732 && val <= 740){
-    lastVal = 'Q';
+    currentVal = 'Q';
   }
   else if (val >= 786 && val <= 793){
-    lastVal = 'B';
+    currentVal = 'B';
   }
   else if (val >= 837 && val <= 843){
-    lastVal = 'C';
+    currentVal = 'C';
   }
   else if (val >= 630 && val <= 640){
-    lastVal = 'D';
+    currentVal = 'D';
   }
   else if (val >= 536 && val <= 545){
-    lastVal = 'E';
+    currentVal = 'E';
   }
   else if (val >= 674 && val <= 681){
-    lastVal = 'F';
+    currentVal = 'F';
   }
   else if (val >= 650 && val <= 660){
-    lastVal = 'G';
+    currentVal = 'G';
   }
   else if (val >= 610 && val <= 620){
-    lastVal = 'H';
+    currentVal = 'H';
   }
   else if (val > 580 && val <= 590){
-    lastVal = 'I';
+    currentVal = 'I';
   }
   else if (val >= 690 && val <= 720){
-    lastVal = 'J';
+    currentVal = 'J';
   }
   else if (val >= 550 && val <= 561){
-    lastVal = 'K';
+    currentVal = 'K';
   }
   else if (val >= 850 && val <= 870){
-    lastVal = 'L';
+    currentVal = 'L';
   }
   else{
     Serial.println(average);
   }
-  
-  if(lastVal != ' ' && prevVal != lastVal && (millis() - lastTimeCall > 500) && skipped){
-    Serial.println(lastVal);
-    //*
+
+  // Si currentVal est rempli, et qu'il n'a pas déjà été pressé dans la demi-seconde qui précède
+  if(currentVal != ' ' && prevVal != currentVal && (millis() - lastTimeCall > 500) && skipped){
+
+    // On peut changer ici les touches du clavier pressées, voir https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers/
+    // pour la liste des modificateurs disponibles (KEY_F13 à KEY_F24 peuvent être intéressants à exploiter :) )
+
+    // On  presse les touches puis on les relâche
     Keyboard.press(KEY_LEFT_GUI);
     Keyboard.press(KEY_LEFT_ALT);
     Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.press(lastVal);
-    Keyboard.releaseAll();//*/
-    prevVal = lastVal;
-    lastVal = ' ';
+    Keyboard.press(currentVal);
+    Keyboard.releaseAll();
+
+    // On sauvegarde la valeur envoyée, ainsi que la dernière fois qu'on a envoyé une combinaison de touches
+    // Pour ne pas spammer la dite combinaison
+    prevVal = currentVal;
+    currentVal = ' ';
     lastTimeCall = millis();
     skipped = !skipped;
+
+
     delay(200);
   } else if (!skipped) {
     skipped = !skipped;
   }
+  // on réinitialise les valeurs pour pouvoir rappeler le même bouton à la suite
   if (millis() - lastTimeCall > 1000){
-    lastVal = ' ';
+    currentVal = ' ';
     prevVal = ' ';
   }
-  prevAnalogVal = analogRead(btns);
 }
